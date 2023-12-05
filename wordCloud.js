a1428fb6d77d073c0db0e138410ecca81e1b4dcf
davegb <davguev@gmail.com>
2023-12-05 05:31:31 -0500

    Fetch, cache, filter, word cloud

--- wordCloud.js
+++ wordCloud.js
@@ -1,51 +1,93 @@
-function wordCloud() {
-  
-    // Your code to create a D3 word cloud goes here
-    // List of words
-    var myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"},
-    {word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"},
-    {word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
-
+function wordCloud(data) {
     // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = document.getElementById('bottom-container-right').clientWidth - margin.left - margin.right,
-    height = document.getElementById('bottom-container-right').clientHeight - margin.top - margin.bottom;
+    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+        width = document.getElementById('bottom-container-right').clientWidth - margin.left - margin.right,
+        height = document.getElementById('bottom-container-right').clientHeight - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     var svg = d3.select('#top-container-left').append("svg")
         .attr('width', '100%')
         .attr('height', '100%')
-    .append("g")
+        .append("g")
         .attr("transform",
             "translate(" + 0 + "," + 0 + ")");
 
-    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+    // Constructs a new cloud layout instance. It runs an algorithm to find the position of words that suits your requirements
     // Wordcloud features that are different from one word to the other must be here
     var layout = d3.layout.cloud()
-    .size([width, height])
-    .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
-    .padding(5)        //space between words
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .fontSize(function(d) { return d.size; })      // font size of words
-    .on("end", draw);
-    layout.start();
+        .size([width, height])
+        .padding(5)        // space between words
+        .rotate(function () { return ~~(Math.random() * 2) * 90; })
+        .fontSize(function (d) { return d.size; })      // font size of words
+        .on("end", draw);
 
-    // This function takes the output of 'layout' above and draw the words
+    updateWordCloud(data);  // Initial rendering
+
+    // This function takes the output of 'layout' above and draws the words
     // Wordcloud features that are THE SAME from one word to the other can be here
     function draw(words) {
-    svg
-        .append("g")
-        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-        .selectAll("text")
+        svg.selectAll("*").remove(); // Clear previous drawings
+
+        var wordLabels = []; // Array to store the labels
+
+        svg.append("g")
+            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+            .selectAll("text")
             .data(words)
-        .enter().append("text")
-            .style("font-size", function(d) { return d.size; })
+            .enter().append("text")
+            .style("font-size", function (d) { return d.size; })
             .style("fill", "#69b3a2")
             .attr("text-anchor", "middle")
             .style("font-family", "Impact")
-            .attr("transform", function(d) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            .attr("transform", function (d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
             })
-            .text(function(d) { return d.text; });
-  }
+            .text(function (d) { return d.text; })
+            .on("click", function (d) {
+                var [clickX, clickY] = d3.mouse(this);
+
+                // Adjust the coordinates to be relative to the #top-container-left div
+                var containerLeft = document.getElementById('top-container-left').getBoundingClientRect().left;
+                var containerTop = document.getElementById('top-container-left').getBoundingClientRect().top;
+
+                var relativeClickX = clickX + containerLeft;
+                var relativeClickY = clickY + containerTop;
+
+                // Add a label when a word is clicked
+                var label = svg.append("g")
+                    .attr("transform", "translate(" + relativeClickX + "," + relativeClickY + ")")
+                    .attr("class", "word-label");
+
+                    label.append("rect")
+                    .attr("width", 30)
+                    .attr("height", 20)
+                    .attr("x", -15)
+                    .attr("y", 5)
+                    .style("fill", "#fff")
+                    .style("stroke", "#000");
+
+                label.append("text")
+                    .attr("x", 0)
+                    .attr("y", 20)
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "12px")
+                    .text("X")
+                    .on("click", function () {
+                        // Remove the label when X is clicked
+                        label.remove();
+                    });
+                wordLabels.push(label);
+            });
+    }
+
+    // Function to update the word cloud with new data
+    function updateWordCloud(newData) {
+        console.log("NEW DATA")
+        console.log(newData)
+        layout.words(newData.map(function (d) { return { text: d.word, size: d.size }; }));
+        layout.start();
+    }
+
+    // Expose the updateWordCloud function if you want to update the word cloud externally
+    this.updateWordCloud = updateWordCloud;
 }
